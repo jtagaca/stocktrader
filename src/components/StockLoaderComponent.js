d9e6b22e0d4a0367f6a4df14d05797b8efac2389
jtagaca <63562250+jtagaca@users.noreply.github.com>
2021-06-19 17:40:02 -0700

    fixed alignment

--- src/components/StockLoaderComponent.js
+++ src/components/StockLoaderComponent.js
@@ -1,55 +1,55 @@
-import React from 'react';
-import { Loading } from './LoadingComponent';
-import NumberFormat from 'react-number-format';
-import { PriceStats } from './PriceStatsComponent';
+import React from "react";
+import { Loading } from "./LoadingComponent";
+import NumberFormat from "react-number-format";
+import { PriceStats } from "./PriceStatsComponent";
+import "../App.css";
 
-export const StockLoader = ({isLoading, isInputFocused, isSearching, description, quote, quoteColor, delta, percentage}) => {
-    return (
-        <div className='col-12 text-center'>
-            <Loading 
-                isLoading={isLoading} 
-                className={`
-                    ${
-                        isLoading 
-                        && 
-                        isSearching 
-                        ? 
-                        'd-none' 
-                        : 
-                        ''
-                    }
+export const StockLoader = ({
+  isLoading,
+  isInputFocused,
+  isSearching,
+  description,
+  quote,
+  quoteColor,
+  delta,
+  percentage,
+}) => {
+  return (
+    <div className="col-12 text-center center ">
+      <Loading
+        isLoading={isLoading}
+        className={`
+                    ${isLoading && isSearching ? "d-none" : ""}
                 `}
-            />
-            
-            <div className={`
+      />
+
+      <div
+        className={`
                 ${
-                    isLoading 
-                    || 
-                    description === '' 
-                    || 
-                    (isInputFocused && isSearching) 
-                    ? 
-                    'd-none' 
-                    : 
-                    ''
+                  isLoading ||
+                  description === "" ||
+                  (isInputFocused && isSearching)
+                    ? "d-none"
+                    : ""
                 }
-            `}>
-                <p className='text-primary title'> {description} </p>
+            `}
+      >
+        <div className="result">
+          <h1 className=" title"> {description} </h1>
 
-                <NumberFormat 
-                    value={quote} 
-                    displayType={'text'} 
-                    thousandSeparator={true} 
-                    decimalScale={2}
-                    prefix={'$'} 
-                    className={`${quoteColor} quote-size`}
-                />
+          <NumberFormat
+            value={quote}
+            style={{ color: "black" }}
+            displayType={"text"}
+            thousandSeparator={true}
+            decimalScale={2}
+            prefix={"$"}
+            className={` quote-size`}
+          />
 
-                <PriceStats
-                    delta={delta} 
-                    percentage={percentage} 
-                />
-            </div>
+          <PriceStats delta={delta} percentage={percentage} />
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
